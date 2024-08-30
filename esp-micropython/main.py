923ffa524c5ec8f1c2539297e68819de4f9d1277
“mirko-geno” <mirkogenovesi24@gmail.com>
2024-08-30 15:02:45 -0300

    FINALMENTE PUDE ENVIAR MENSAJES

--- esp-micropython/main.py
+++ esp-micropython/main.py
@@ -1,13 +1,13 @@
+import micropython
 import machine
-import time
+import select
+import sys
 
-# Configuración del puerto serie
-uart = machine.UART(0, baudrate=9600)  # Usa UART0 con baudrate de 9600
 led = machine.Pin(2, machine.Pin.OUT)  # El LED interno está en el pin GPIO 2 en el ESP8266
+micropython.kbd_intr(-1)
 
 while True:
-    if uart.any():  # Verifica si hay datos en el buffer serial
-        uart.read(1)  # Lee 1 byte (el dato enviado)
+  while sys.stdin in select.select([sys.stdin], [], [], 0)[0]:        
+    ch = sys.stdin.read(1)
+    if ch:
         led.value(not led.value())  # Alterna el estado del LED
-        print("Dato recibido, LED alternado")
-        time.sleep(0.1)  # Añade un pequeño retraso para evitar posibles rebotes
--- modules/serial_transmitter.py
+++ modules/serial_transmitter.py
@@ -6,7 +6,7 @@ ser = serial.Serial('/dev/ttyUSB0', 9600)  # Cambia 9600 por la velocidad adecua
 
 try:
     while True:
-        ser.write(b'1')
+        ser.write(b'25')
         print("Dato enviado")
         time.sleep(1)
 
