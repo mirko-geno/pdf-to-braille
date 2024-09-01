f5b92b336fa824c4e31738bde769b61fde6c3242
mirko-geno <mirkogenovesi24@gmail.com>
2024-09-01 19:52:22 -0300

    probably finished

--- esp-micropython/main.py
+++ esp-micropython/main.py
@@ -1,10 +1,18 @@
+# main
 from time import sleep
-import sys
 import select
+import sys
 
 while flash_button.value():
     if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:  # Only reads if theres something in the port
         byte = sys.stdin.buffer.read(1)  # Reads a byte from the serial port
+        data_int = int.from_bytes(byte, 'big')
         # print(f'Byte received: {byte}')
         internal_led.value(not internal_led.value())
+
+        for i in range(OUT_LEDS):
+            led_state = (data_int >> i) & 1  # Desplaza el bit i hacia la derecha y extrae el bit menos significativo
+            print(f'{i}. led_state: {led_state}')
+            # led_pins[i].value(led_state)  # Establece el estado del LED basado en el bit correspondiente
+
     sleep(0.1)
--- modules/serial_receiver.py
+++ modules/serial_receiver.py
@@ -9,14 +9,14 @@ internal_led = Pin(2, Pin.OUT)
 flash_button = Pin(0, Pin.IN, Pin.PULL_UP)
 
 led_pins = [Pin(10, Pin.OUT),
-            Pin(9, Pin.OUT),
             Pin(16, Pin.OUT),
             Pin(5, Pin.OUT),
-
             Pin(4, Pin.OUT),
-            Pin(2, Pin.OUT),
+
             Pin(14, Pin.OUT),
-            Pin(12, Pin.OUT),]
+            Pin(12, Pin.OUT),
+            Pin(13, Pin.OUT),
+            Pin(15, Pin.OUT)]
 
 
 # main
@@ -27,12 +27,13 @@ import sys
 while flash_button.value():
     if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:  # Only reads if theres something in the port
         byte = sys.stdin.buffer.read(1)  # Reads a byte from the serial port
+        data_int = int.from_bytes(byte, 'big')
         # print(f'Byte received: {byte}')
-        if byte == b'\x98':
-            internal_led.value(not internal_led.value())
+        internal_led.value(not internal_led.value())
 
         for i in range(OUT_LEDS):
-            led_state = (byte >> i) & 1  # Desplaza el bit i hacia la derecha y extrae el bit menos significativo
-            led_pins[i].value(led_state)  # Establece el estado del LED basado en el bit correspondiente
+            led_state = (data_int >> i) & 1  # Desplaza el bit i hacia la derecha y extrae el bit menos significativo
+            print(f'{i}. led_state: {led_state}')
+            # led_pins[i].value(led_state)  # Establece el estado del LED basado en el bit correspondiente
 
     sleep(0.1)
