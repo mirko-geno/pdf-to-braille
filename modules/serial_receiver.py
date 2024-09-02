ebf7d717854b5178af9ca919012aebe50e2c5597
“mirko-geno” <mirkogenovesi24@gmail.com>
2024-09-02 16:57:23 -0300

    pin update

--- modules/serial_receiver.py
+++ modules/serial_receiver.py
@@ -8,15 +8,16 @@ OUT_LEDS = const(8)
 internal_led = Pin(2, Pin.OUT)
 flash_button = Pin(0, Pin.IN, Pin.PULL_UP)
 
+
 led_pins = [Pin(10, Pin.OUT),
+            Pin(12, Pin.OUT),
+            Pin(13, Pin.OUT),
+            Pin(15, Pin.OUT),
+
             Pin(16, Pin.OUT),
             Pin(5, Pin.OUT),
             Pin(4, Pin.OUT),
-
-            Pin(14, Pin.OUT),
-            Pin(12, Pin.OUT),
-            Pin(13, Pin.OUT),
-            Pin(15, Pin.OUT)]
+            Pin(14, Pin.OUT)]
 
 
 # main
