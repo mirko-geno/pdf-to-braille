a82af88f497819caed0e8bfc7d74ef5f81e4b961
mirko-geno <mirkogenovesi24@gmail.com>
2024-09-02 21:06:54 -0300

    update

--- modules/serial_receiver.py
+++ modules/serial_receiver.py
@@ -4,20 +4,28 @@ from micropython import kbd_intr, const
 kbd_intr(-1)
 
 OUT_LEDS = const(8)
+BITS = const([14, 15, 4, 13, 5, 16, 12, 10])
+
+'''
+[BIT 7 = PIN 10] [BIT 5 = PIN 16]
+[BIT 6 = PIN 12] [BIT 4 = PIN 5 ]
+[BIT 3 = PIN 13] [BIT 2 = PIN 4 ]
+[BIT 1 = PIN 15] [BIT 0 = PIN 14]
+
+'''
 
 internal_led = Pin(2, Pin.OUT)
 flash_button = Pin(0, Pin.IN, Pin.PULL_UP)
 
 
-led_pins = [Pin(10, Pin.OUT),
-            Pin(12, Pin.OUT),
-            Pin(13, Pin.OUT),
-            Pin(15, Pin.OUT),
-
-            Pin(16, Pin.OUT),
-            Pin(5, Pin.OUT),
-            Pin(4, Pin.OUT),
-            Pin(14, Pin.OUT)]
+led_pins = [Pin(BITS[7], Pin.OUT),
+            Pin(BITS[6], Pin.OUT),
+            Pin(BITS[5], Pin.OUT),
+            Pin(BITS[4], Pin.OUT),
+            Pin(BITS[3], Pin.OUT),
+            Pin(BITS[2], Pin.OUT),
+            Pin(BITS[1], Pin.OUT),
+            Pin(BITS[0], Pin.OUT)]
 
 
 # main
