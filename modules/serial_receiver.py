b62bf1d5b3124ed16a4ed7828fd60e0aefe7d9c9
mirko-geno <mirkogenovesi24@gmail.com>
2024-09-08 20:21:51 -0300

    list comprehension for leds assignment

--- modules/serial_receiver.py
+++ modules/serial_receiver.py
@@ -4,7 +4,7 @@ from micropython import kbd_intr, const
 kbd_intr(-1)
 
 OUT_LEDS = const(8)
-BITS = [14, 15, 4, 13, 5, 16, 12, 1]
+PINS = [14, 15, 4, 13, 5, 16, 12, 1]
 '''
 [BIT 7 = PIN 14] [BIT 5 = PIN 4 ]
 [BIT 6 = PIN 15] [BIT 4 = PIN 13]
@@ -14,15 +14,7 @@ BITS = [14, 15, 4, 13, 5, 16, 12, 1]
 '''
 internal_led = Pin(2, Pin.OUT)
 flash_button = Pin(0, Pin.IN, Pin.PULL_UP)
-
-led_pins = [Pin(BITS[0], Pin.OUT),
-            Pin(BITS[1], Pin.OUT),
-            Pin(BITS[2], Pin.OUT),
-            Pin(BITS[3], Pin.OUT),
-            Pin(BITS[4], Pin.OUT),
-            Pin(BITS[5], Pin.OUT),
-            Pin(BITS[6], Pin.OUT),
-            Pin(BITS[7], Pin.OUT)]
+led_pins = [Pin(pin, Pin.OUT) for pin in PINS]
 
 for led in led_pins:
     led.value(0)
