9ede7c6df40344e46801484beb3a460b1d96290a
“mirko-geno” <mirkogenovesi24@gmail.com>
2024-10-18 16:14:13 -0300

    ttyACM for pi pico

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from modules.reader_remade import Reader
 
 def main():
     print('Inciando programa...\nLeyendo archivo PDF')    
-    pdf_reader = Reader(path='Yo, robot - Isaac Asimov.pdf', reading_freq=2, port='/dev/ttyUSB0', baudrate=115200)
+    pdf_reader = Reader(path='sample_pdfs/Yo, robot - Isaac Asimov.pdf', reading_freq=3, port='/dev/ttyACM0', baudrate=230400)
 
     print('PDF leído exitosamente\n Iniciando lectura...\n\n\n')
     pdf_reader.read()
